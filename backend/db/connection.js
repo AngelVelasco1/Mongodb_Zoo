f124c3795b0683fbefb2c98231cfc430bcde1488
DavidAndrésRuedaChacónCAMPUS <42551054+davidruc@users.noreply.github.com>
2023-08-30 13:37:22 -0500

    Changes related with the connection to the database (#5)
    * feat: :sparkles: changes related to the connection with the data base
    
    * fix: :bug: Fix changes with the fail pull request
    
    * fix: :bug: fix problems with the branches
    
    * fix: :bug: fix A bug

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,28 +1,28 @@
-import { MongoClient } from 'mongodb';
-import { CONFIG } from '../src/utils/config.js'
+import config from "../src/utils/config.js";
+import { MongoClient } from "mongodb";
 
-const uri = `mongodb+srv://${CONFIG.user}:${CONFIG.password}@cluster0.tfk8jyc.mongodb.net/${CONFIG.db}`;
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
-    retryWrites: true
-};
-const client = await MongoClient.connect(uri, options);
+let dbConnection = null;
 
-export const getConx = async () => {
+export async function connect(){
+    if(dbConnection){
+        return dbConnection;
+    };
     try {
-        return client.db();
-    } 
-    catch (err) {
-        return {
-            status: 500,
-            err: err
-        }
+        const uri = `mongodb+srv://${config.user}:${config.password}@cluster0.mqhexgk.mongodb.net/${config.db}`;
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        };
+        const cliente = await MongoClient.connect(uri, options);
+        dbConnection = cliente.db();
+        return dbConnection;
+    } catch (error) {
+        return {status: 500, message: error}
     }
-}
+};
 
-export const endConx = async () => {
-    await client.close();
-    console.log('Connection closed');
-}
-
+export const collectionGen = async (colecction) =>{
+    const db = await connect();
+    const newCollection = db.collection(colecction);
+    return newCollection;
+};
