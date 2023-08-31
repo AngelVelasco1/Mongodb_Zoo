7ec221cff914f0715c322ba0b46ec5eb0751f81b
davidruc <davidarueda18@gmail.com>
2023-08-31 15:36:20 -0500

    feat: :sparkles: creation of the entities with the crud of the project

---
+++ backend/src/collections/Services.js
@@ -0,0 +1,67 @@
+import { collectionGen } from "../../db/connection.js";
+
+class Services{
+    /* aqui ingreso los datos como está construida la colección en la base de datos como una guia únicamente, estos valores no afectan el código, simplemente son una guia visual. */
+    _id;
+    id;
+    shop;
+    product_name;
+    id_staff;
+    date;
+    amount;
+    price;
+    devolutions;
+    reason;
+    refund;
+    constructor(){};
+
+    async connection(){
+        try {
+            const result = await collectionGen("Services");
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    };
+    async getServices(id){
+        try {
+            const connect = await this.connection();
+            if(!id) return await connect.find({}).toArray();
+            return await connect.aggregate([{$match: { "id": parseInt(id)}}]);
+        } catch (error) {
+            throw error;
+        }
+    };
+    async postServices(data){
+        try {
+            const connect = await this.connection();
+            /* arreglar el tema del siguienteID */
+            const result = await connect.insertOne(data);
+            return result;
+        } catch (error) {
+            throw error; 
+        }
+    };
+    async updateServices(id, data){
+        try {
+            const connect = await this.connection();
+            const result = await connect.updateOne(
+                { "id": parseInt(id)},
+                { $set: data }
+            )
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    };
+    async deleteServices(id){
+        try {
+            const connect = await this.connection();
+            const result = await connect.deleteOne({"id": parseInt(id)});
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+};
+export {Services};
