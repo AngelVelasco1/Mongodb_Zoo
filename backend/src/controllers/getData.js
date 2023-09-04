e8c24b3289edb34a918f02fec4e3f6adc0ce6c64
Angel Velasco <angelgg2020@outlook.com>
2023-09-03 21:37:34 -0500

    feat: :sparkles: Implementing specific consults controllers and routes

--- backend/src/controllers/getData.js
+++ backend/src/controllers/getData.js
@@ -1,5 +1,7 @@
 import { getServiceHabitat, getServiceAnimals, getServiceEmergencies, getServiceServices, getServiceStaff, getServiceTacoShop  } from "../services/get.js";
-
+import { getServiceAnimalsByEntryY, getServiceAnimalsDeath, getServiceStaffAnimals, getServiceHabitatAnimals, getServiceAnimalsNoVcunes } from "../services/get.js";
+import { getServiceDevolutions, getServiceLossMoney, getServiceBalanceDay, getServiceSellsMonth } from "../services/get.js";
+import {  getServiceStaffBySalary, getServiceStaffOlder, getServiceTacoInOrder, getServiceBestMonth, getServiceEmergenciesByVet, getServiceIncidentsByAnimal, getServiceEmergenciesBetweenDates } from "../services/get.js"; 
 export const getHabitatsController = async (req, res) => {
     try {
         const {id} = req.query;
@@ -59,6 +61,164 @@ export const getTacoShopController = async (req, res) => {
         res.status(500).send(err)
     }
 }
+
+
+
+export const AnimalsByEntryYController = async (req, res) => {
+    try {
+        const {year} = req.query;
+        const animals = await getServiceAnimalsByEntryY(year);
+        res.status(200).json(animals);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const AnimalsDeathController = async (req, res) => {
+    try {
+        const animals = await getServiceAnimalsDeath();
+        res.status(200).json(animals);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const StaffAnimalsController = async (req, res) => {
+    try {
+        const {id} = req.query;
+        const staff = await getServiceStaffAnimals(id);
+        res.status(200).json(staff);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const HabitatAnimalsController = async (req, res) => {
+    try {
+        const {id} = req.query;
+        const habitat = await getServiceHabitatAnimals(id);
+        res.status(200).json(habitat);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const getAnimalsNoVacunesController = async (req, res) => {
+    try {
+        const animals = await getServiceAnimalsNoVcunes();
+        res.status(200).json(animals);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const DevolutionsController = async (req, res) => {
+    try {
+        const devolutions = await getServiceDevolutions();
+        res.status(200).json(devolutions);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const LossMoneyController = async (req, res) => {
+    try {
+        const {month} = req.query;
+        const lossMoney = await getServiceLossMoney(month);
+        res.status(200).json(lossMoney);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const BalanceDayController = async (req, res) => {
+    try {
+        const {date} = req.query;
+        const balanceDay = await getServiceBalanceDay(date);
+        res.status(200).json(balanceDay);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const SellsMonthController = async (req, res) => {
+    try {
+        const {month} = req.query;
+        const sellsMonth = await getServiceSellsMonth(month);
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const StaffBySalaryController = async (req, res) => {
+    try {
+        const {salary} = req.query;
+        const sellsMonth = await getServiceStaffBySalary(salary);
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export  const StaffOlderController = async (req, res) => {
+    try {
+        const sellsMonth = await getServiceStaffOlder();
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const TacoInOrderController = async (req, res) => {
+    try {
+        const sellsMonth = await getServiceTacoInOrder();
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+} 
+
+export const BestMonthController = async (req, res) => {
+    try {
+        const sellsMonth = await getServiceBestMonth();
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const EmergenciesByVetController = async (req, res) => {
+    try {
+        const {id_vet} = req.query;
+        const sellsMonth = await getServiceEmergenciesByVet(id_vet);
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const IncidentsByAnimalController = async (req, res) => {
+    try {
+        const {id} = req.query;
+        const sellsMonth = await getServiceIncidentsByAnimal(id);
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+export const BetweenDatesController = async (req, res) => {
+    try {
+        const {start, end} = req.query;
+        const sellsMonth = await getServiceEmergenciesBetweenDates(start, end);
+        res.status(200).json(sellsMonth);
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+
 
 export const notAllowed = (req, res) => {
     res.status(405).send({message: "Method not allowed in this version"});
--- backend/src/routes/getData.js
+++ backend/src/routes/getData.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getHabitatsController, getAnimalsController, getEmergenciesController, getServicesController, getStaffsController, getTacoShopController } from "../controllers/getData.js";
+import { AnimalsByEntryYController, AnimalsDeathController, StaffAnimalsController, HabitatAnimalsController, getAnimalsNoVacunesController, DevolutionsController, LossMoneyController, BalanceDayController, SellsMonthController, StaffBySalaryController, StaffOlderController, TacoInOrderController, BestMonthController, EmergenciesByVetController, IncidentsByAnimalController, BetweenDatesController  } from "../controllers/getData.js";
 import passportHelper from "../helpers/passport.js";
 import routesVersioning from "express-routes-versioning";
 
@@ -36,6 +37,88 @@ export const getInitRoute = () => {
     app.get("/tacoShop", version({
         "^1.0.0": getTacoShopController,
         "^2.0.0": getTacoShopController,
+    })),
+
+    app.get("/animalsByEntryY", version({
+        "^1.0.0": AnimalsByEntryYController,
+        "^2.0.0": AnimalsByEntryYController,
+    }));
+
+    app.get("/animalsDeath", version({
+        "^1.0.0": AnimalsDeathController,
+        "^2.0.0": AnimalsDeathController,
     }))
+
+    app.get("/staffAnimals", version({
+        "^1.0.0": StaffAnimalsController,
+        "^2.0.0": StaffAnimalsController,
+    }))
+
+    app.get("/habitatAnimals", version({
+        "^1.0.0": HabitatAnimalsController,
+        "^2.0.0": HabitatAnimalsController,
+    }))
+
+    app.get("/animalsNoVacunes", version({
+        "^1.0.0": getAnimalsNoVacunesController,
+        "^2.0.0": getAnimalsNoVacunesController,
+    }))
+
+    app.get("/devolutions", version({
+        "^1.0.0": DevolutionsController,
+        "^2.0.0": DevolutionsController,
+    }))
+
+    app.get("/lossMoney", version({
+        "^1.0.0": LossMoneyController,
+        "^2.0.0": LossMoneyController,
+    }))
+
+    app.get("/balanceDay", version({
+        "^1.0.0": BalanceDayController,
+        "^2.0.0": BalanceDayController,
+    }))
+
+    app.get("/sellsMonth", version({
+        "^1.0.0": SellsMonthController,
+        "^2.0.0": SellsMonthController,
+    }))
+
+    app.get("/staffBySalary", version({
+        "^1.0.0": StaffBySalaryController,
+        "^2.0.0": StaffBySalaryController,
+    }))
+
+    app.get("/staffOlder", version({
+        "^1.0.0": StaffOlderController,
+        "^2.0.0": StaffOlderController,
+    }))
+
+    app.get("/tacoInOrder", version({
+        "^1.0.0": TacoInOrderController,
+        "^2.0.0": TacoInOrderController,
+    }))
+
+    app.get("/bestMonth", version({
+        "^1.0.0": BestMonthController,
+        "^2.0.0": BestMonthController,
+    }))
+
+    app.get("/emergenciesByVet", version({
+        "^1.0.0": EmergenciesByVetController,
+        "^2.0.0": EmergenciesByVetController,
+    }))
+
+    app.get("/incidentsByAnimal", version({
+        "^1.0.0": IncidentsByAnimalController,
+        "^2.0.0": IncidentsByAnimalController,
+    }))
+
+    app.get("/betweenDates", version({
+        "^1.0.0": BetweenDatesController,
+        "^2.0.0": BetweenDatesController,
+    }))
+
+    
     
 }
