d2be160c20c707c2ce0eb2b8f7714ed3c4bb9486
Angel Velasco <angelgg2020@outlook.com>
2023-09-03 19:42:17 -0500

    fix: :adhesive_bandage: Fix basic logic of controllers

--- backend/src/controllers/putData.js
+++ backend/src/controllers/putData.js
@@ -2,8 +2,9 @@ import { putServiceHabitat, putServiceAnimals, putServiceEmergencies, putService
 
 export const putHabitatController = async (req, res) => {
     try {
+        const data = req.body;
         const { id } = req.query;
-        const habitats = await putServiceHabitat(req.body, id)
+        const habitats = await putServiceHabitat(data, id)
         res.status(201).json(habitats);
     } catch(err) {
         res.status(500).send(err)
@@ -12,8 +13,9 @@ export const putHabitatController = async (req, res) => {
 
 export const putAnimalsController = async (req, res) => {
     try {
+        const data = req.body;
         const { id } = req.query;
-        const animals = await putServiceAnimals(req.body, id)
+        const animals = await putServiceAnimals(data, id)
         res.status(201).json(animals);
     } catch(err) {
         res.status(500).send(err)
@@ -22,8 +24,9 @@ export const putAnimalsController = async (req, res) => {
 
 export const putEmergenciesController = async (req, res) => {
     try {
+        const data = req.body;
         const { id } = req.query;
-        const emergencies = await putServiceEmergencies(req.body, id)
+        const emergencies = await putServiceEmergencies(data, id)
         res.status(201).json(emergencies);
     } catch(err) {
         res.status(500).send(err)
@@ -32,8 +35,9 @@ export const putEmergenciesController = async (req, res) => {
 
 export const putServicesController = async (req, res) => {
     try {
+        const data = req.body;
         const { id } = req.query;
-        const services = await putServiceServices(req.body, id)
+        const services = await putServiceServices(data, id)
         res.status(201).json(services);
     } catch(err) {
         res.status(500).send(err)
@@ -42,8 +46,9 @@ export const putServicesController = async (req, res) => {
 
 export const putStaffsController = async (req, res) => {
     try {
+        const data = req.body;
         const { id } = req.query;
-        const staffs = await putServiceStaff(req.body, id)
+        const staffs = await putServiceStaff(data, id)
         res.status(201).json(staffs);
     } catch(err) {
         res.status(500).send(err)
@@ -52,8 +57,9 @@ export const putStaffsController = async (req, res) => {
 
 export const putTacoShopController = async (req, res) => {
     try {
+        const data = req.body;
         const { id } = req.query;
-        const tacoShop = await putServiceTacoShop(req.body, id)
+        const tacoShop = await putServiceTacoShop(data, id)
         res.status(201).json(tacoShop);
     } catch(err) {
         res.status(500).send(err)
